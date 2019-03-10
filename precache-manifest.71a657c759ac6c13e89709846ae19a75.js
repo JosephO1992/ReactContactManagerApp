11416204be0e4129af81d9662fee8748124af907
Joe O <josephraoreilly@gmail.com>
2019-03-10 14:33:09 +0800

    Updates

---
+++ precache-manifest.71a657c759ac6c13e89709846ae19a75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "459f4d4d9844b0033bc1",
+    "url": "/ReactContactManagerApp/static/js/runtime~main.459f4d4d.js"
+  },
+  {
+    "revision": "9c413da11303120a46d9",
+    "url": "/ReactContactManagerApp/static/js/main.9c413da1.chunk.js"
+  },
+  {
+    "revision": "518972bdb5b18455c8db",
+    "url": "/ReactContactManagerApp/static/js/2.518972bd.chunk.js"
+  },
+  {
+    "revision": "9c413da11303120a46d9",
+    "url": "/ReactContactManagerApp/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "518972bdb5b18455c8db",
+    "url": "/ReactContactManagerApp/static/css/2.4288e879.chunk.css"
+  },
+  {
+    "revision": "8db6f41cfbac7bfde050893fa111e59a",
+    "url": "/ReactContactManagerApp/index.html"
+  }
+];
